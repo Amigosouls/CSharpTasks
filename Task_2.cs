a519429162b59aa5424c3c1df8cec02dbd331d07
Amigosouls <amigosouls022@gmail.com>
2023-07-27 16:41:41 +0530

    Task 3 finished

---
+++ Task_2.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTasks
+{
+    internal class Task_2
+    {
+        
+        static void Main(string[] args)
+        {
+            int row, col, trineeCount=2;
+            string traineeName;
+            int traineeAge;
+            int traineeNo;
+            string traineeAddress;
+            int bonus;
+
+            int[,] createdArr;
+            Console.WriteLine("Enter The number of Rows:");
+            row = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number of Columns:");
+            col = Convert.ToInt32(Console.ReadLine());
+            Task2_1 twoDArray = new Task2_1(row,col);
+            createdArr = twoDArray.getArrayValues();
+            Console.WriteLine("The Array is................");
+            twoDArray.showArray(createdArr);
+            twoDArray.convertArray(createdArr);
+            twoDArray.jaggedArray();
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    twoDArray.calculateBonus(out traineeName, out traineeAge, out traineeNo, out traineeAddress,out bonus);
+            //    Console.WriteLine("Emp Name:" + traineeName);
+            //    Console.WriteLine("Emp Age:" + traineeAge);
+            //    Console.WriteLine("Emp No:" + traineeNo);
+            //    Console.WriteLine("Emp Address:" + traineeAddress);
+            //    Console.WriteLine("Emp Bonus:" + bonus);
+            //}
+            twoDArray.getEmployee();
+            
+        }
+    }
+    public class Task2_1
+    {
+        public int arrrow;
+        public int arrcol;
+
+        public Task2_1(int row, int col)
+        {
+            arrrow = row;
+            arrcol = col;
+        }
+        public int[,] getArrayValues()
+        {
+            int[,] userArr = new int[arrrow, arrcol];
+            for (int i = 0; i < arrrow; i++)
+            {
+                for (int j = 0; j < arrcol; j++)
+                {
+                    Console.Write($"Enter the {i} \t {j} th value");
+                    userArr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            return userArr;
+        }
+        public void showArray(int[,] arr)
+        {
+            for (int i = 0; i < arrrow; i++)
+            {
+                for (int j = 0; j < arrcol; j++)
+                {
+                    Console.Write(arr[i, j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+        }
+        public void convertArray(int[,] inpArr)
+        {
+            int count = arrcol * arrrow;
+            int[] oneDArray = new int[count];
+            int iterator = 0;
+            for (int i = 0; i < arrrow; i++)
+            {
+                for (int j = 0; j < arrcol; j++)
+                {
+                    oneDArray[iterator] = inpArr[i, j];
+                    iterator++;
+                }
+            }
+            Console.WriteLine("Converted Array is......");
+            for (int i = 0; i < oneDArray.Length; i++)
+            {
+                Console.Write(oneDArray[i] + "\t");
+            }
+        }
+        public void jaggedArray()
+        {
+            string[][,] names = new string[3][,]
+            {
+                new string[,]{{"hello","Hola" },{"hi","bye" } },
+                new string[,]{ {"darshan","vicky" },{"hemanth","sharon" } },
+                new string[,] {{"C#","Angular" },{".NET","React" } }
+
+            };
+            Console.WriteLine("Jagged Array.......");
+            for(int i=0; i < names.Length; i++)
+            {
+                int x = 0;
+                //Console.WriteLine(names[i].Length);
+                for(int j=0; j < names[i].GetLength(x); j++)
+                {
+                    for (int k = 0; k < names[j].Rank; k++)
+                    {
+                        Console.Write(names[i][j, k] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                x++;
+                Console.WriteLine();
+            }
+        }
+
+        public void showTrainees(params object[] trainee)
+        {
+            Console.WriteLine("Trainee details...");
+            Console.WriteLine(trainee.Length);
+            foreach (object obj in trainee)
+            {
+                Console.WriteLine($"{obj.ToString()}");
+            }
+        }
+        public void calculateBonus(out string EmpName, out int empAge, out int empNo, out string empAddress,out int bonus )
+        {
+            string name;
+            int age;
+            int salary;
+            string address;
+            Console.WriteLine("Emp Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Emp Age");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Emp Salary:");
+            salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Emp Address");
+            address = Console.ReadLine();
+            EmpName = name;
+            empAge = age;
+            empNo = (salary % 10) + salary;
+            empAddress = address;
+            bonus = salary % 10;
+          
+
+        }
+        
+
+
+        public void getEmployee()
+        {
+            int traineeCount = 2;
+            string traineeName;
+            int traineeAge;
+            string traineeAddress;
+
+            object []traineeDetails;
+            for (int i = 0; i < traineeCount; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    Console.WriteLine("Trainee Name:");
+                    traineeName = Console.ReadLine();
+                    Console.WriteLine("Trainee Age");
+                    traineeAge = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Trainee Address");
+                    traineeAddress = Console.ReadLine();
+                    //Object trainee  = new Object( { traineeName, traineeAge, traineeAddress });
+                    //traineeDetails+=trainee;
+                }
+
+
+               
+            }
+
+            //showTrainees(traineeDetails);
+
+
+        }
+        public void showTrainees(params object[][,] trainee)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for(int j = 0;j < 1; j++)
+                {
+                    Console.WriteLine(trainee[i]);
+                }
+               
+            }
+        }
+     }
+ }
+
---
+++ Task_3.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTasks
+{
+    internal class Task_3
+    {
+       
+        static void Main(string[] args)
+        {
+            int count = 0;
+            
+            char type;
+            Console.WriteLine("Enter the no of elements");
+            count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ArrayType:");
+            type=Convert.ToChar( Console.ReadLine());
+
+            if (type == 'i')
+            {
+                Generics<int,char> obj1 = new Generics<int,char>();
+                int[] arr = new int[count];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                obj1.reverseArray(arr);
+                char a, b;
+                Console.WriteLine("Enter the value for a :");
+                a = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Enter the value for b:");
+                b = Convert.ToChar(Console.ReadLine());
+                obj1.swapNumbers(a, b);
+
+
+            }
+            else
+            {
+                Generics<char,int> obj2 = new Generics<char,int>();
+                char[] arr = new char[count];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = Convert.ToChar(Console.ReadLine());
+                }
+                obj2.reverseArray(arr);
+                int a, b;
+                Console.WriteLine("Enter the value for a :");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the value for b:");
+                b = Convert.ToInt32(Console.ReadLine());
+                obj2.swapNumbers(a, b);
+            }
+            CombinationGenerics<int, char, string> obj3 = new CombinationGenerics<int, char, string>(2, 'c', "Heyah!!");
+            Task3_2 obj4 = new Task3_2();
+            obj4.nullOperations();
+            obj4 = new Task3_2(10,23,"Srikanth","Male");
+            obj4.showTrainees(obj4);
+        }
+    }
+    internal class Generics<T,T1>
+    {
+        public void reverseArray(T[] array)
+        {
+            Array.Reverse(array);
+            foreach (T item in array)
+            {
+                Console.Write(item + "\t");
+            }
+        }
+        public void swapNumbers(T1 val1, T1 val2)
+        {
+            T1 val3;
+            val3= val1;
+            val1= val2;
+            val2 = val3;
+            Console.WriteLine("value of A:" + val1);
+            Console.WriteLine( "value of B:"+val2);
+        }
+
+    }
+    internal class CombinationGenerics<T1, T2, T3>
+    {
+        T1 val1;
+        T2 val2;
+        T3 val3;
+        public CombinationGenerics(T1 val1, T2 val2, T3 val3)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+            this.val3 = val3;
+            Console.WriteLine($"Type of {val1} is {typeof(T1)}");
+            Console.WriteLine($"Type of {val2} is {typeof(T2)}");
+            Console.WriteLine($"Type of {val3} is {typeof(T3)}");
+        }
+      
+    }
+    internal class Task3_2
+    {
+        int id;
+        int age;
+        string name;
+        string gender;
+        public Task3_2(int id, int age, string name, string gender)
+        {
+            this.id = id;
+            this.age = age;
+            this.name = name;
+            this.gender = gender;
+        }
+        public Task3_2()
+        {
+
+        }
+        public void showTrainees(Task3_2 trainee)
+        {
+            Console.WriteLine($"{trainee.name} with ID no {trainee.id} of age{trainee.age} is a {trainee.gender}");
+        }
+        public void nullOperations()
+        {
+            ArrayList nullArr = new ArrayList();
+            Console.WriteLine("Enter 5 values, press enter to add null");
+            for (int i = 0; i < 5; i++)
+            {
+                var j = Console.ReadLine();
+                if(j == "")
+                {
+                    nullArr.Add(null);
+                    continue;
+                }
+                nullArr.Add(j);
+
+            }
+            foreach (object obj in nullArr)
+            {
+                if(obj == null)
+                {
+                    Console.WriteLine("i have a value");
+                    continue;
+                }
+                Console.WriteLine(obj.ToString());
+            }
+        }
+    }
+}
